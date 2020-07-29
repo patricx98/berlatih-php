6dbaa5e7bc27222c04d9e6f343a0d57b2e184d6c
Muhammad Najib <najibm682@gmail.com>
2020-07-29 23:45:45 +0700

    palindrome-angka berhasil

---
+++ palindrome-angka.php
@@ -0,0 +1,23 @@
+<?php
+
+function palindrome_angka($angka) {
+    do {
+        $angka++;
+        $panjang = strlen($angka);  //panjang karakter
+        $split = str_split($angka); //pecah pecah text
+        $text = "";                 //set variable penampung data pembalik
+        for ($i = $panjang-1; $i >= 0; $i--) {
+            $text .= $split[$i];         
+        }
+    }while ($text != $angka);
+    echo($text."<br>");
+}
+
+// TEST CASES
+echo palindrome_angka(8); // 9
+echo palindrome_angka(10); // 11
+echo palindrome_angka(117); // 121
+echo palindrome_angka(175); // 181
+echo palindrome_angka(1000); // 1001
+
+?>
